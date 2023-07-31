0c977d89459214f2a3f73aea94fe8861126ce60b
dylanmsql <masquelierdylan1999@gmail.com>
2023-07-31 10:30:49 +0200

    Add Socials component

---
+++ src/components/Socials.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {faGithub, faLinkedinIn} from "@fortawesome/free-brands-svg-icons";
+    import Fa from "svelte-fa";
+</script>
+<ul class="social">
+  <li><a href="https://github.com/dylanmsql"><Fa icon="{faGithub}" /></a></li>
+  <li><a href="https://www.linkedin.com/in/dylan-masquelier-5741b0185/"><Fa icon="{faLinkedinIn}" /></a></li>
+</ul>
+
+<style>
+    li {
+        list-style: none;
+        margin: 1rem 0;
+    }
+
+    li a {
+        font-size: 1.3rem;
+    }
+
+    a:hover {
+        color: #31e3c3;
+        transition: color 0.3s ease-in-out;
+    }
+</style>
