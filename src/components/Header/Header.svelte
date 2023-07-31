4720ba7e7cddf82a1694582a73ec0c2c93398fcb
dylanmsql <masquelierdylan1999@gmail.com>
2023-07-31 16:45:20 +0200

    add navItems array to generate all link

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
-    import Fa from "svelte-fa";
-    import {faGithub, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
     import Profile from "./Profile.svelte";
     import Socials from "./Socials.svelte";
+
+    const navItems = [
+        {
+            name: "About",
+            link: "#about",
+        },
+        {
+            name: "Skills",
+            link: "#skills",
+        },
+        {
+            name: "Works",
+            link: "#works",
+        },
+        {
+            name: "Contact",
+            link: "#contact",
+        },
+        {
+            name: "Resume",
+            link: "/src/assets/Resume.pdf",
+        },
+    ];
 </script>
 
 <header>
   <Profile />
   <nav>
-    <a href="#about">About</a>
-    <a href="#skills">Skills</a>
-    <a href="#works">Works</a>
-    <a href="#contact">Contact</a>
-    <a href="/src/assets/Resume.pdf">Resume</a>
+    {#each navItems as navItem}
+      <a href={navItem.link}>{navItem.name}</a>
+    {/each}
   </nav>
   <Socials />
 </header>
@@ -26,6 +45,7 @@
         align-items: center;
         height: 100vh;
         width: 10rem;
+        background-color: #1a1a1a;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
     }
 
