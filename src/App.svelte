e3c01a77cbe43060f466d4abd5ee0ae949aefe9f
dylanmsql <masquelierdylan1999@gmail.com>
2023-07-31 10:32:41 +0200

    remove useless comment

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
-  import { Router, Route, Link } from 'svelte-routing';
-  import Skill from "./pages/About.svelte";
-  import Home from "./pages/Home.svelte";
   import Header from "./components/Header.svelte";
-  export let url: string = '';
 </script>
-
-<!--<Router url="{url}">-->
-<!--  <nav>-->
-<!--    <Link to="/">Home</Link>-->
-<!--    <Link to="/skill">Skill</Link>-->
-<!--  </nav>-->
-<!--  <Route path="/" component="{Home}"/>-->
-<!--  <Route path="/skill" component="{Skill}"/>-->
-<!--</Router>-->
 
 <main>
   <Header />
