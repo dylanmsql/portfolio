d97278a3b4ac54e81b6fedbacd2420b5cdcc0c7e
dylanmsql <masquelierdylan1999@gmail.com>
2023-07-31 10:30:12 +0200

    init vite+svelte+ts project

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Router, Route, Link } from 'svelte-routing';
+  import Skill from "./pages/About.svelte";
+  import Home from "./pages/Home.svelte";
+  import Header from "./components/Header.svelte";
+  export let url: string = '';
+</script>
+
+<!--<Router url="{url}">-->
+<!--  <nav>-->
+<!--    <Link to="/">Home</Link>-->
+<!--    <Link to="/skill">Skill</Link>-->
+<!--  </nav>-->
+<!--  <Route path="/" component="{Home}"/>-->
+<!--  <Route path="/skill" component="{Skill}"/>-->
+<!--</Router>-->
+
+<main>
+  <Header />
+</main>
+
+<style scope>
+    a {
+        font-weight: 500;
+        color: #646cff;
+        text-decoration: inherit;
+    }
+    a:hover {
+        color: #535bf2;
+    }
+
+    h1 {
+        font-size: 3.2em;
+        line-height: 1.1;
+    }
+</style>
