ab4a55a5a072694daf65d18607d27514e47baf45
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-01 14:35:05 +0200

    first idea of layout for the about section

--- src/components/Content/About.svelte
+++ src/components/Content/About.svelte
@@ -1,3 +1,41 @@
 <section id="about">
-  <h1>About</h1>
+  <img src="/src/assets/image/77782703.jpeg" alt="face">
+  <div class="about-content">
+    <h1>About me</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
 </section>
+
+<style>
+    #about {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: auto;
+        max-width: 1000px;
+    }
+
+    #about img {
+        width: 300px;
+        height: 300px;
+        border-radius: 50%;
+        margin: 50px;
+    }
+
+    .about-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        /*padding: 50px 0;*/
+    }
+
+    .about-content h1 {
+        font-size: 50px;
+        font-weight: 700;
+        margin-bottom: 20px;
+    }
+</style>
