40f3e67ce5e3fd2b36aff3decc54a2318949a2f7
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-07 15:51:42 +0200

    change color theme and remove useless style rules

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import Header from "./components/Header/Header.svelte";
+  import Header from "./layouts/Header.svelte";
   import About from "./components/Content/About.svelte";
-  import Content from "./components/Content/Content.svelte";
+  import Content from "./layouts/Content.svelte";
   import Skills from "./components/Content/Skills.svelte";
   import Works from "./components/Content/Works.svelte";
   import Contact from "./components/Content/Contact.svelte";
@@ -16,19 +16,3 @@
     <Contact />
   </Content>
 </main>
-
-<style>
-    a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-    }
-    a:hover {
-        color: #535bf2;
-    }
-
-    h1 {
-        font-size: 3.2em;
-        line-height: 1.1;
-    }
-</style>
