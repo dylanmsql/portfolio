f5dac77b298311a76f6a9a6a37944b189f9063d7
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-07 16:51:06 +0200

    add Button Component

---
+++ src/components/Button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let label: string = 'Click me';
+  export let onClick: (e:Event) => void = e => {};
+</script>
+
+<button onclick="{onClick}">
+  {label}
+</button>
+
+<style lang="scss">
+  button {
+    border: 3px solid #EBB257;
+    color: #EBB257;
+    cursor: pointer;
+    font-size: 1em;
+    padding: 0.5em 1em;
+    transition: background 0.3s ease-in-out;
+
+    &:hover {
+      background: #EBB257;
+      color: #fff;
+    }
+  }
+</style>
