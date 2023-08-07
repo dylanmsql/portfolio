e13c250f044bb0768b1e4d24a2f43896bfa30a34
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-07 16:51:17 +0200

    add Divider component

---
+++ src/components/Divider.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let size: number = 50;
+</script>
+
+<hr class="solid" style={`width:${size}px`}>
+
+<style>
+    hr.solid {
+        border-top: 3px solid #EBB257;
+    }
+</style>
