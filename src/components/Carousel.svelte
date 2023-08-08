694dde6cc1cbfaaba89d29dc74ae624b14e06858
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-08 15:42:48 +0200

    add carousel of component

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+    import Fa from "svelte-fa";
+    import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons";
+    import Dots from "./Dots.svelte";
+
+    export let components: Array<any>;
+
+    let index = 0;
+    $: current = components[index];
+
+    const prev = (e) => {
+        e.preventDefault();
+        const newIndex = index - 1;
+        if (newIndex >= 0) {
+            index = newIndex;
+        }
+    };
+
+    const next = (e) => {
+        e.preventDefault();
+        const newIndex = index + 1;
+        if (newIndex <= components.length - 1) {
+            index = newIndex;
+        }
+    };
+</script>
+
+<div class="carousel">
+  {#key index}
+    <div class="carousel-content">
+      <svelte:component this={current}/>
+    </div>
+  {/key}
+  <div class="carousel-panel">
+    <button on:click={prev}>
+      <Fa icon={faArrowLeft}/>
+    </button>
+    <Dots itemCount={components.length} current={index} />
+    <button on:click={next}>
+      <Fa icon={faArrowRight}/>
+    </button>
+  </div>
+</div>
+
+<style>
+    .carousel {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+
+    .carousel-content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+    }
+
+    .carousel-panel {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 100px;
+    }
+
+    .carousel-panel button {
+        width: 50px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        border: solid 3px #EBB257;
+        cursor: pointer;
+        background-color: transparent;
+    }
+</style>
---
+++ src/components/Dots.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let itemCount: number;
+    export let current;
+</script>
+
+<div class="dots-container">
+  {#each Array(itemCount) as _, i}
+    <span class="dots" class:active={current === i}></span>
+  {/each}
+</div>
+
+<style lang="scss">
+  .dots-container {
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  .dots {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    border: solid 2px #EBB257;
+  }
+
+  .active {
+    transform: scale(1.5);
+    background-color: #EBB257;
+  }
+</style>
