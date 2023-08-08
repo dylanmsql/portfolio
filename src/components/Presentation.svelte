72e6826714814cbaa2905ceabd543263f7ac4654
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-08 15:54:32 +0200

    compute the age

--- src/components/Presentation.svelte
+++ src/components/Presentation.svelte
@@ -1,8 +1,16 @@
+<script lang="ts">
+  const birthDate = new Date(1999, 8, 14);
+  const now = new Date();
+
+  const isPassedYet = now.getMonth() > birthDate.getMonth() || (now.getMonth() === birthDate.getMonth() && now.getDate() >= birthDate.getDate());
+  const age = now.getFullYear() - birthDate.getFullYear() - (isPassedYet ? 0 : 1);
+</script>
+
 <div class="about">
   <h1>Who am I ?</h1>
   <div class="info">
     <p class="name"><b>Name : </b> Dylan Masquelier</p>
-    <p class="duration"><b>Age : </b> 23</p>
+    <p class="duration"><b>Age : </b> {age}</p>
     <p class="address"><b>Address : </b> Lille, France</p>
     <p class="mail"><b>Mail : </b> dmasquelier@ippon.fr</p>
   </div>
