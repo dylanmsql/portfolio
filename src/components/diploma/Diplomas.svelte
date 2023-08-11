0d72016b66c37d71d8c46585a6ecccd57cb341c4
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-11 14:13:06 +0200

    Refacto diplomas

--- src/components/diploma/Diplomas.svelte
+++ src/components/diploma/Diplomas.svelte
@@ -3,24 +3,24 @@
 
   const contents = [
     {
-      start: '2017',
-      end: '2019',
+      start: 2017,
+      end: 2019,
       diploma: 'Brevet de technicien supérieur',
       speciality: 'Système Numerique Informatique et réseaux',
       school: 'Lycée Gustave Eiffel',
       location: 'Armentières',
     },
     {
-      start: '2019',
-      end: '2021',
+      start: 2019,
+      end: 2021,
       diploma: 'Licence',
       speciality: 'Informatique',
       school: 'Université de Lille',
       location: 'Villeneuve d\'Ascq',
     },
     {
-      start: '2021',
-      end: '2023',
+      start: 2021,
+      end: 2023,
       diploma: 'Master',
       speciality: 'Internet des objets',
       school: 'Université de Lille',
