fd9a72fe1f605b2cc82f80d895488d09c6d6b137
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-11 16:04:22 +0200

    add Carousel to sort different skill

--- src/layouts/section/Skills.svelte
+++ src/layouts/section/Skills.svelte
@@ -1,37 +1,8 @@
 <script>
+  import Carousel from '../../components/Carousel.svelte';
+  import LanguageSkill from '../../components/Skill/LanguageSkill.svelte';
+
   const skills = [
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/html5-40-1175193.png',
-      name: 'HTML',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png',
-      name: 'CSS',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png',
-      name: 'JavaScript',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/c-57-1175191.png',
-      name: 'C',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/c-4-226082.png',
-      name: 'C++',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/csharp-1-1175241.png',
-      name: 'C#',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/java-60-1174953.png',
-      name: 'Java',
-    },
-    {
-      image: 'https://cdn.iconscout.com/icon/free/png-256/python-2-226051.png',
-      name: 'Python',
-    },
     {
       image: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
       name: 'NodeJS',
@@ -78,12 +49,9 @@
 <section id='skills'>
   <h1>Skills</h1>
   <div class='skill'>
-    {#each skills as skill}
-      <div class='skill-card'>
-        <img src='{skill.image}' alt='{skill.name}'>
-        <h2>{skill.name}</h2>
-      </div>
-    {/each}
+    <Carousel>
+      <LanguageSkill />
+    </Carousel>
   </div>
 </section>
 
