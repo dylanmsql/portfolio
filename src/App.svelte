0dad91cfadc50eaef7dc4aa9c835cc079937cb69
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-25 12:15:05 +0200

    Refacto project structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang='ts'>
   import Header from './layouts/Header.svelte';
-  import About from './layouts/section/About.svelte';
+  import About from './layouts/section/about/About.svelte';
   import Content from './layouts/Content.svelte';
-  import Skills from './layouts/section/Skills.svelte';
+  import Skills from './layouts/section/skills/Skills.svelte';
   import Works from './layouts/section/Works.svelte';
   import Contact from './layouts/section/Contact.svelte';
 </script>
