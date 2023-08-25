1b7032436a98eb02d1124a492e33b27e0afe8536
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-25 12:02:53 +0200

    Add link card

---
+++ src/components/LinkCard.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Fa from 'svelte-fa';
+
+  export let contact;
+</script>
+
+
+<a href={contact.link}>
+  {#if contact.icon}
+    <Fa icon={contact.icon} />
+  {/if}
+  <p>{contact.name}</p>
+</a>
+
+<style>
+    a {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin: 1rem;
+        padding: 1rem;
+        border-radius: 5px;
+        background-color: #3c3d41;
+        cursor: pointer;
+        gap: 1rem;
+        transition: all 0.2s ease-in-out;
+    }
+
+    a:hover {
+        background-color: #2c2d31;
+    }
+
+    p {
+        margin: 0;
+        color: #ebb257;
+    }
+
+
+</style>
