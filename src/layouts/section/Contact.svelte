fd973aae0096a9bf3a097ef3c6e001425085e69b
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-25 12:03:10 +0200

    implement link card to contact section

--- src/layouts/section/Contact.svelte
+++ src/layouts/section/Contact.svelte
@@ -1,15 +1,41 @@
 <script>
-  const sendMail = () => {
-    window.open('mailto:masquelierdylan1999@gmail.com');
-  };
+  import Fa from 'svelte-fa';
+  import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+  import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+  import LinkCard from '../../components/LinkCard.svelte';
+
+  const contacts = [
+    {
+      name: 'GitHub',
+      icon: faGithub,
+      link: 'https://github.com/dylanmsql',
+    },
+    {
+      name: 'LinkedIn',
+      icon: faLinkedin,
+      link: 'https://www.linkedin.com/in/dylan-masquelier-1b1b3a1b3/',
+    },
+    {
+      name: 'masquelierdylan1999@gmail.com',
+      icon: faEnvelope,
+      link: 'mailto:masquelierdylan1999@gmail.com',
+    },
+    {
+      name: '+33 6 75 14 86 81',
+      icon: faPhone,
+      link: 'tel:+33675148681',
+    }
+  ];
 </script>
 
 <section id='contact'>
-  <h1>Contact</h1>
-  <p>My inbox is always open.</p>
-  <button on:click|preventDefault={sendMail}>
-    Say Hello
-  </button>
+  <h1>Ways to contact me</h1>
+  <p>Feel free to contact me if you have any questions or if you want to work with me.</p>
+  <div class='links'>
+    {#each contacts as contact}
+      <LinkCard {contact} />
+    {/each}
+  </div>
 </section>
 
 <style>
@@ -23,26 +49,21 @@
     }
 
     #contact h1 {
-        font-size: 3rem;
+        font-size: 2rem;
+        text-align: center;
     }
 
     #contact p {
-        font-size: 1.5rem;
+        font-size: 1.2rem;
+        text-align: center;
     }
 
-    #contact button {
-        margin: 2rem;
-        padding: 1rem 2rem;
-        border: none;
-        border-radius: 5px;
-        background-color: #1d1d1d;
-        color: #fff;
-        font-size: 1.5rem;
-        cursor: pointer;
-    }
-
-    #contact button:hover {
-        background-color: #fff;
-        color: #1d1d1d;
+    .links {
+        margin: 2rem 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
     }
 </style>
