176e9e9666b5a09fd9d7353d255656fcf712e580
dylanmsql <masquelierdylan1999@gmail.com>
2023-08-29 12:23:01 +0200

    change tab name of each category

--- src/layouts/section/skills/Skills.svelte
+++ src/layouts/section/skills/Skills.svelte
@@ -4,36 +4,48 @@
   let activeTab = 'webdev';
 
   const categories = {
-    'webdev': [
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/html5-40-1175193.png', name: 'HTML' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png', name: 'CSS' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png', name: 'JavaScript' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/react-4-1175110.png', name: 'React' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/vue-282497.png', name: 'Vue' },
-      {
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/langfr-220px-Svelte_Logo.svg.png',
-        name: 'Svelte',
-      },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png', name: 'NodeJS' },
-    ],
-    'softwaredev': [
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/c-programming-569564.png', name: 'C' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/c-programming-569564.png', name: 'C++' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/java-60-1174953.png', name: 'Java' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/python-2-226051.png', name: 'Python' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/c-programming-569564.png', name: 'C#' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/dot-net-1-1175179.png', name: '.NET Core' },
-    ],
-    'databases': [
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/mysql-19-1174939.png', name: 'MySQL' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/mongodb-3-1175138.png', name: 'MongoDB' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/postgresql-11-1175122.png', name: 'PostgreSQL' },
-    ],
-    'devops': [
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/git-225996.png', name: 'Git' },
-      { image: 'https://cdn.iconscout.com/icon/free/png-256/docker-226091.png', name: 'Docker' },
-    ],
-  }
+    'webdev': {
+      name: 'Web Development',
+      content: [
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/html5-40-1175193.png', name: 'HTML' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png', name: 'CSS' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png', name: 'JavaScript' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/react-4-1175110.png', name: 'React' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/vue-282497.png', name: 'Vue' },
+        {
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/langfr-220px-Svelte_Logo.svg.png',
+          name: 'Svelte',
+        },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png', name: 'NodeJS' },
+      ],
+    },
+    'softwaredev': {
+      name: 'Software Development',
+      content: [
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/c-programming-569564.png', name: 'C' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/c-programming-569564.png', name: 'C++' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/java-60-1174953.png', name: 'Java' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/python-2-226051.png', name: 'Python' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/c-programming-569564.png', name: 'C#' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/dot-net-1-1175179.png', name: '.NET Core' },
+      ],
+    },
+    'databases': {
+      name: 'Databases',
+      content: [
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/mysql-19-1174939.png', name: 'MySQL' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/mongodb-3-1175138.png', name: 'MongoDB' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/postgresql-11-1175122.png', name: 'PostgreSQL' },
+      ],
+    },
+    'devops': {
+      name: 'DevOps & Version Control',
+      content: [
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/git-225996.png', name: 'Git' },
+        { image: 'https://cdn.iconscout.com/icon/free/png-256/docker-226091.png', name: 'Docker' },
+      ],
+    },
+  };
 
   console.log(categories[activeTab]);
 </script>
@@ -41,21 +53,21 @@
 <section id='skills'>
   <div class='skills-wrapper'>
     <h1>Skills</h1>
-    <div class="browser-frame">
-      <div class="browser-tabs">
+    <div class='browser-frame'>
+      <div class='browser-tabs'>
         {#each Object.keys(categories) as category}
           <button
-            class="tab" class:active={activeTab === category}
+            class='tab' class:active={activeTab === category}
             on:click={() => activeTab = category}
           >
-            {category}
+            {categories[category].name}
           </button>
         {/each}
       </div>
 
       <!-- Content Area -->
-      <div class="content-area">
-        {#each categories[activeTab] as skill}
+      <div class='content-area'>
+        {#each categories[activeTab].content as skill}
           <SkillCard {skill} />
         {/each}
       </div>
